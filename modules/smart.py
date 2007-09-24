84207483cc8a8d00d047ab3c15e2f15cb194884c
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-09-24 12:01:38 -0400

    Add basic module for checking smart status of drives.

---
+++ modules/smart.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+## 
+## Grabs status from SMART to see if your hard drives are ok
+## Returns in the format of (return code, [line1, line2, line3,...])
+##
+## Copyright 2007, Red Hat, Inc
+## Michael DeHaan <mdehaan@redhat.com>
+##
+## This software may be freely redistributed under the terms of the GNU
+## general public license.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+##
+
+# other modules
+import subprocess
+
+# our modules
+from codes import *
+from modules import func_module
+
+# =================================
+
+class SmartModule(func_module.FuncModule):
+    def __init__(self):
+        self.methods = {
+            "info"    : self.info,
+        }
+        func_module.FuncModule.__init__(self)
+
+    def info(self,flags="-q onecheck"):
+        """
+        Returns a struct of hardware information.  By default, this pulls down
+        all of the devices.  If you don't care about them, set with_devices to
+        False.
+        """
+
+        flags.replace(";","") # prevent stupidity
+
+        cmd = subprocess.Popen("/usr/sbin/smartd %s" % flags,stdout=subprocess.PIPE,shell=True)
+        data = cmd.communicate()[0]
+
+        results = []       
+
+        for x in data.split("\n"):
+            tokens = x.split()
+            results.append(tokens)
+
+        return (cmd.returncode, results)
+
+methods = SmartModule()
+register_rpc = methods.register_rpc
+
+
+
