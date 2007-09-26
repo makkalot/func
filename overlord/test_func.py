c85f1a8272b665ed810ca05b5b31d35a30f00600
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-09-26 15:22:35 -0400

    Generalize test code.

--- overlord/test_func.py
+++ overlord/test_func.py
@@ -5,6 +5,7 @@
 
 import xmlrpclib
 import sys
+import socket
 
 TEST_GETATTR = True
 TEST_PROCESS = False
@@ -15,9 +16,9 @@ TEST_SMART = True
 
 if TEST_GETATTR:
    import func.overlord.client as func_client
-   #print func_client.Client("*").hardware.info()
-   #print func_client.Client("*").run("hardware","info",[])
-   print func_client.Client("mdehaan.rdu.redhat.com",noglobs=True).test.add("1","2")
+   print func_client.Client("*").hardware.info()
+   print func_client.Client("*").run("hardware","info",[])
+   print func_client.Client(socket.gethostname(),silent=True,noglobs=True).test.add("1","2")
    sys.exit(1)
 
 # get a connecton (to be replaced by client lib logic)
