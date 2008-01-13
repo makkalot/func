fac34430c569e763264424e01a66fbda79d89e5c
Michael DeHaan <mdehaan@redhat.com>
2008-01-13 15:48:23 -0500

    This is why I normally don't program on weekends, kids.

--- func/minion/modules/test.py
+++ func/minion/modules/test.py
@@ -18,7 +18,7 @@ class Test(func_module.FuncModule):
     def ping(self):
         return 1
 
-    def sleep(t):
+    def sleep(self,t):
         """
         Sleeps for t seconds, and returns time of day.
         Simply a test function for trying out async and threaded voodoo.
