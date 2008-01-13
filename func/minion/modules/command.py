4ff11f4a5e883bcba11f021ae3d42dd318cc6396
Michael DeHaan <mdehaan@redhat.com>
2008-01-13 10:22:07 -0500

    Add Steve's match to check if a command exists.

--- func/minion/modules/command.py
+++ func/minion/modules/command.py
@@ -1,5 +1,6 @@
 # Copyright 2007, Red Hat, Inc
 # James Bowes <jbowes@redhat.com>
+# Steve 'Ashcrow' Milner <smilner@redhat.com>
 #
 # This software may be freely redistributed under the terms of the GNU
 # general public license.
@@ -20,7 +21,8 @@ class Command(func_module.FuncModule):
 
     def __init__(self):
         self.methods = {
-                "run" : self.run
+                "run" : self.run,
+                "exists" : self.exists,
         }
         func_module.FuncModule.__init__(self)
 
@@ -33,6 +35,17 @@ class Command(func_module.FuncModule):
         cmdref = sub_process.Popen(command.split(),stdout=sub_process.PIPE,stderr=sub_process.PIPE, shell=False)
         data = cmdref.communicate()
         return (cmdref.returncode, data[0], data[1])
+
+    def exists(self, command):
+        """
+        Checks to see if a command exists on the target system(s).
+        """
+        cmdref = sub_process.Popen(['/usr/bin/which', command], stdout=sub_process.PIPE)
+        data = cmdref.communicate()
+        if cmdref.returncode == 0:
+            return True
+        return False
+
 
 methods = Command()
 register_rpc = methods.register_rpc
