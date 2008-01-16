2d51ab4d3cf01f567ee45e66a97fc247693bbccd
Adrian Likins <alikins@grimlock.devel.redhat.com>
2008-01-16 11:54:15 -0500

    pychecker/pyflakes
    remove unused imports

--- func/overlord/client.py
+++ func/overlord/client.py
@@ -13,17 +13,14 @@
 ## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 ##
 
-import optparse
 import sys
 import glob
-import pprint
 
 from func.commonconfig import CMConfig
 from func.config import read_config, CONFIG_FILE
 
 import sslclient
 
-import command
 import forkbomb
 import jobthing
 
