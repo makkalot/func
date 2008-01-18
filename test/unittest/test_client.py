a6efb74bfb7f13a2c3149dda21c09e1f8ce4208e
Adrian Likins <alikins@grimlock.devel.redhat.com>
2008-01-18 17:18:05 -0500

    do some refactoring
    talk to localhost by default instead of "grimlock.devel.redhat.com"

--- test/unittest/test_client.py
+++ test/unittest/test_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import socket
 import unittest
 import xmlrpclib
 
@@ -9,7 +10,8 @@ import func.overlord.client as fc
 
 
 class BaseTest:
-    th = "grimlock.devel.redhat.com"
+    # assume we are talking to localhost
+    th = socket.gethostname()
     def __init__(self):
         pass
 
@@ -19,16 +21,20 @@ class BaseTest:
     def test_module_version(self):
         mod = getattr(self.client, self.module)
         result = mod.module_version()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_module_api_version(self):
         mod = getattr(self.client, self.module)
         result = mod.module_api_version()        
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_module_description(self):
         mod = getattr(self.client, self.module)
         result = mod.module_description()
+        self.assert_on_fault(result)
+
+    # we do this all over the place...
+    def assert_on_fault(self, result):
         assert type(result[self.th]) != xmlrpclib.Fault
 
     test_module_version.intro = True
@@ -40,16 +46,14 @@ class TestTest(BaseTest):
     module = "test"
     def test_add(self):
         result = self.client.test.add(1,5)
-
+        self.assert_on_fault(result)
         assert result[self.th] == 6
 
     def test_add_string(self):
         result = self.client.test.add("foo", "bar")
-
+        self.assert_on_fault(result)
         assert result[self.th] == "foobar"
 
-    def tearDown(self):
-        pass
 
 
 class TestCommand(BaseTest):
@@ -57,11 +61,13 @@ class TestCommand(BaseTest):
     def test_echo(self):
         result = self.client.command.run("echo -n foo")
 
+        self.assert_on_fault(result)
         assert result[self.th][1] == "foo"
 
     def test_rpm(self):
         result = self.client.command.run("rpm -q func")
 
+        self.assert_on_fault(result)
         assert result[self.th][1].split("-")[0] == "func"
 
 
@@ -81,7 +87,9 @@ class TestCopyfile(BaseTest):
         fb = open(self.fn,"r").read()
         data = xmlrpclib.Binary(fb)
         result = self.client.copyfile.copyfile(self.dest_fn, data)
+        self.assert_on_fault(result)
         assert result[self.th]  == 0
+        
  
     def test_checksum(self):
         self.create_a_file()
@@ -89,6 +97,7 @@ class TestCopyfile(BaseTest):
         data = xmlrpclib.Binary(fb)
         result = self.client.copyfile.copyfile(self.dest_fn, data)
         result = self.client.copyfile.checksum(self.dest_fn)
+        self.assert_on_fault(result)
         assert result[self.th] == "b36a8040e44c16605d7784cdf1b3d9ed04ea7f55"
         
 
@@ -96,19 +105,20 @@ class TestHardware(BaseTest):
     module = "hardware"
     def test_inventory(self):
         result = self.client.hardware.inventory()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_halinfo(self):
         result = self.client.hardware.hal_info()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_info(self):
         result = self.client.hardware.info()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
+
 
     def test_info_no_devices(self):
         result = self.client.hardware.info(False)
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
 class TestFileTracker(BaseTest):
     fn = "/etc/hosts"
@@ -116,18 +126,19 @@ class TestFileTracker(BaseTest):
     def test_track(self):
         result = self.client.filetracker.track(self.fn)
         assert result[self.th] == 1
+        self.assert_on_fault(result)
 
     def test_inventory(self):
         result = self.client.filetracker.track(self.fn)
         result = self.client.filetracker.inventory(False)
-        assert type(result[self.th]) != xmlrpclib.Fault 
+        self.assert_on_fault(result)
         assert result[self.th][0][0] == "/etc/hosts"
         assert result[self.th][0][3] == 0
 
     def test_untrack(self):
         result = self.client.filetracker.track(self.fn)
         result = self.client.filetracker.untrack(self.fn)
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
         result_inv = self.client.filetracker.inventory(False)
         tracked_files = result_inv[self.th]
         for i in tracked_files:
@@ -139,9 +150,7 @@ class TestMount(BaseTest):
     module = "mount"
     def test_mount_list(self):
         result = self.client.mount.list()
-        #FIXME: I probably should make the test for xmlrpclib faults a bit
-        #       more automagic
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     # INSERT some clever way to test mount here
 
@@ -150,8 +159,8 @@ class TestNetworkTest(BaseTest):
     module = "networktest"
     def test_ping(self):
         result = self.client.networktest.ping(self.th, "-c", "2")
-        assert type(result[self.th]) != xmlrpclib.Fault
-    
+        self.assert_on_fault(result)
+
     def test_ping_bad_arg(self):
          result = self.client.networktest.ping(self.th)
          # this should give us a FuncException
@@ -159,34 +168,34 @@ class TestNetworkTest(BaseTest):
          
     def test_netstat(self):
         result = self.client.networktest.netstat("-n")
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_traceroute(self):
         result = self.client.networktest.traceroute(self.th)
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_dig(self):
         result = self.client.networktest.dig("redhat.com")
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_isportopen_closed_port(self):
         result = self.client.networktest.isportopen(self.th, 34251)
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_isportopen_open_port(self):
         result = self.client.networktest.isportopen(self.th, 51234)
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
 
 class TestProcess(BaseTest):
     module = "process"
     def test_info(self):
         result = self.client.process.info()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_mem(self):
         result = self.client.process.mem()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     # FIXME: how to test kill/pkill? start a process with
     #        command and then kill it?
@@ -196,20 +205,20 @@ class TestService(BaseTest):
     module = "service"
     def test_inventory(self):
         result = self.client.service.inventory()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
     
     def test_get_enabled(self):
         result = self.client.service.get_enabled()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_get_running(self):
         result = self.client.service.get_running()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     def test_get_status(self):
         running_data = self.client.service.get_running()[self.th]
         result = self.client.service.status(running_data[0][0])
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
         assert result[self.th] == 0
 
         #FIXME: whats a good way to test starting/stoping services without
@@ -219,37 +228,37 @@ class TestRpm(BaseTest):
     module = "rpms"
     def test_inventory(self):
         result = self.client.rpms.inventory()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
 
 class TestSmart(BaseTest):
     module = "smart"
     def test_info(self):
         result = self.client.smart.info()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
     
 
 class TestYum(BaseTest):
     module = "yumcmd"
     def test_check_update(self):
         result = self.client.yumcmd.check_update()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
         print result
 
 class TestSystem(BaseTest):
     module = "system"
     def test_list_methods(self):
         result = self.client.system.list_methods()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
     
     def test_listMethods(self):
         result = self.client.system.listMethods()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
     
     def test_list_modules(self):
         result = self.client.system.list_modules()
-        assert type(result[self.th]) != xmlrpclib.Fault
+        self.assert_on_fault(result)
 
 
     #FIXME: we really should just implement these for the system stuff
