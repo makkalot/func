6f8e551a5562fc4e6f9b10e6c9ff6a0e5ff373c6
makkalot <makkalot@gmail.com>
2008-08-04 15:38:36 +0300

    selecy all uncheck all js code

--- funcweb/funcweb/static/javascript/utils.js
+++ funcweb/funcweb/static/javascript/utils.js
@@ -1,15 +1,25 @@
-function checkAll(form_element){
-
+function check_all(class_name){
+    var check_boxes = getElementsByTagAndClassName('input',class_name);
+    for (var check_element in check_boxes){
+        if (compare(check_boxes[check_element].checked,false)==0){
+            check_boxes[check_element].checked = true;
+        }
+    }
 }
 
-function uncheckAll(form_element){
-    
+function uncheck_all(class_name){
+    var check_boxes = getElementsByTagAndClassName('input',class_name);
+    for (var check_element in check_boxes){
+        if (compare(check_boxes[check_element].checked,true)==0)
+            check_boxes[check_element].checked = false;
+    }
+   
 }
 
-function checkController(form_element,check_element){
-    if (check_element.checked == 1){
-        checkAll(form_element);
+function checkController(class_name,check_element){
+    if (check_element.checked == true){
+        check_all(class_name);
     }
     else
-        uncheckAll(form_element);
+        uncheck_all(class_name);
 }
