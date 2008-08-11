5b149b28ab84031a0a85bf080b968aca4fbdf33b
makkalot <makkalot@gmail.com>
2008-08-11 09:42:53 +0300

    a simple addition in export method api

--- func/minion/func_arg.py
+++ func/minion/func_arg.py
@@ -50,7 +50,9 @@ class ArgCompatibility(object):
                 'boolean':(),
                 'float':('range','min','max'),
                 'hash':('validator',),
+                'hash**':('validator',),
                 'list':('validator',),
+                'list*':('validator',),
             }
 
 
