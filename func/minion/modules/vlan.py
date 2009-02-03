13c1b4b3c513bcb72b0f8f9e7223bc7ab7a0e226
Jasper Capel <capel@stone-it.com>
2009-02-03 14:39:47 +0100

    Forgot to add pydoc description to vlan module.

--- func/minion/modules/vlan.py
+++ func/minion/modules/vlan.py
@@ -17,6 +17,10 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301  USA
 
+"""
+Func module for VLAN management
+"""
+
 import func_module
 import os, re
 from certmaster.config import BaseConfig, Option, ListOption
