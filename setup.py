427cf8d82192222d15f478a87bcb8ee05c4d2deb
makkalot <makkalot@fedorabig.localdomain>
2009-03-03 23:05:52 +0200

    update setup.py with new stuff

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ if __name__ == "__main__":
                             "%s/overlord/modules" % NAME,
                             "%s/minion/modules" % NAME,
                             "%s/minion/facts" % NAME,
+                            "%s/minion/facts/modules/" % NAME,
                             "%s/yaml" % NAME,
                             # FIXME if there's a clean/easy way to recursively
                             # find modules then by all means do it, for now
