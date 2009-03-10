aed21de2feca44cefb55383202159b306c969e44
makkalot <makkalot@fedorabig.localdomain>
2009-03-10 22:55:20 +0200

    async tests for overlord_proxy will add more like test_client.py

--- test/unittest/test_facts_overlord.py
+++ test/unittest/test_facts_overlord.py
@@ -23,6 +23,12 @@ class TestOverlordQueryProxy(object):
                                     nforks=self.nforks,
                                     async=self.async)
 
+        self.async_overlord = fc.Overlord(self.th,
+                                    nforks=self.nforks,
+                                    async=True)
+
+
+
         self.negated_q = FuncLogicQuery(
                 (~Q(a=True,b=True)| Q(c=True,b=False))&
                 Q(e=True,f=False)
@@ -38,18 +44,30 @@ class TestOverlordQueryProxy(object):
         Testing the serialzation thing
         """
         self.tmp_proxy = OverlordQueryProxy(overlord_obj=self.overlord,fact_query=self.query)
-        print self.tmp_proxy.serialize_query()
+        #print self.tmp_proxy.serialize_query()
         
         self.tmp_proxy = OverlordQueryProxy(overlord_obj=self.overlord,fact_query=self.negated_q)
-        print self.tmp_proxy.serialize_query()
+        #print self.tmp_proxy.serialize_query()
     
         self.tmp_proxy = OverlordQueryProxy(overlord_obj=self.overlord,fact_query=self.negated_q2)
-        print self.tmp_proxy.serialize_query()
+        #print self.tmp_proxy.serialize_query()
     
 
 
-    #def test_chain_send(self):
-    #    self.tmp_proxy = OverlordQueryProxy(overlord_obj=self.overlord,fact_query=self.query)
-    #    res = self.tmp_proxy.service.status("httpd")
-    #    print res
+    def test_chain_send(self):
+        query = FuncLogicQuery(Q(runlevel__lt=6,runlevel__gt=2)) 
+        self.tmp_proxy = OverlordQueryProxy(overlord_obj=self.overlord,fact_query=query)
+        res = self.tmp_proxy.hardware.info()
+        print res
+
+    def test_async_chain_send(self):
+        query = FuncLogicQuery(Q(runlevel__lt=6,runlevel__gt=2)) 
+        self.tmp_proxy = OverlordQueryProxy(overlord_obj=self.async_overlord,fact_query=query)
+        res = self.tmp_proxy.hardware.info()
+        print res
+        import time
+        time.sleep(5)
+        print "The final stuff is ",self.tmp_proxy.job_status(res)
+
+
 
