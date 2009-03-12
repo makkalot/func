9ef234400be43d3230aa9f2943c4cc71d362fa86
makkalot <makkalot@fedorabig.localdomain>
2009-03-12 16:33:02 +0200

    add more facts to hardware module for better testing and demonstration

--- func/minion/facts/modules/hardware.py
+++ func/minion/facts/modules/hardware.py
@@ -24,4 +24,48 @@ class HardwareFacts(fact_module.BaseFactModule):
     run_level.description = "Shows the runlevel of the system"
     run_level.usage = "Can be used with all keywords"
 
+    def os_name(self):
+        """
+        Gives back the os name of the system
+        """
+        return str(self.host.os)
+    
+    #for easier acces be creful should be unique
+    os_name.tag = "os"
+    os_name.description = "Shows the os of the system"
+    os_name.usage = "Can be used with all keywords better with string related"
+
+    
+    def cpu_vendor(self):
+        """
+        The cpu vendor easy one
+        """
+        return str(self.host.cpuVendor)
+
+    cpu_vendor.tag = "cpuvendor"
+    cpu_vendor.description = "Shows the cpu_vendor of the system"
+    cpu_vendor.usage = "Can be used with all keywords better with string related"
+
+    
+    def cpu_model(self):
+        """
+        Cpu model
+        """
+        return str(self.host.cpuModel)
+ 
+    cpu_model.tag = "cpumodel"
+    cpu_model.description = "Shows the cpu_model of the system"
+    cpu_model.usage = "Can be used with all keywords better with string related"
+
+    def kernel_version(self):
+        """
+        Kernel version
+        """
+        return str(self.host.kernelVersion)
+    
+    kernel_version.tag = "kernel"
+    kernel_version.description = "Shows the kernel version of the system"
+    kernel_version.usage = "Can be used with all keywords"
+
+
 
