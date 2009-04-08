9e3d7b9f878c3a1ad6568f05c4b4e040fbba0acc
Adrian Likins <alikins@redhat.com>
2009-04-08 15:35:20 -0400

    more config unittest cases

--- test/unittest/test_config.py
+++ test/unittest/test_config.py
@@ -39,6 +39,25 @@ class ConfigFileValues:
     def __init__(self):
         pass
 
+class ConfigFileDefaultValues(ConfigFileValues):
+    option = "this is an option"
+    int_option = 1111
+    bool_option = True
+    bool_option_false = False
+    bool_option_true = True
+    list_option = ["one", "two", "three"]
+    url_list_option = ["https://fedorahosted.org/func/", "http://www.redhat.com"]
+    float_option = 3.14159
+
+class ConfigFileWriteValues(ConfigFileValues):
+    option = "this is an option written to the config file"
+    int_option = 3333
+    bool_option = True
+    list_option = ["eleven", "twelve", "thirteen"]
+    url_list_option = ["http://www.example.com", "http://www.python.org"]
+    float_option = 3.33333
+
+
 class ConfigTest(config.BaseConfig):
     option = config.Option("this is an option")
     int_option = config.IntOption(1111)
@@ -97,6 +116,51 @@ class TestConfig:
 #    def test_config_bytes_option(self):
 #        print self.exp.bytes_option, self.cfg.bytes_option
 #        assert self.exp.bytes_option == self.cfg.bytes_option
+
+
+class TestDefaultConfig(TestConfig):
+    module = "config"
+    exp = ConfigFileDefaultValues()
+
+    def setUp(self):
+        self.cfg = ConfigTest()
+
+
+# load from a file, write to the config object, test that it's
+# correct. Note that this does not actually save the file
+class TestConfigSet(TestConfig):
+    module = "config"
+    exp = ConfigFileWriteValues()
+
+    def setUp(self):
+        TestConfig.setUp(self)
+        
+        self.cfg.option = self.exp.option
+        self.cfg.bool_option = self.exp.bool_option
+        self.cfg.int_option = self.exp.int_option
+        self.cfg.list_option = self.exp.list_option
+        self.cfg.url_list_option = self.exp.url_list_option
+        self.cfg.float_option = self.exp.float_option
+
+
+#read the file from one config file, write it to another, reread, and then test
+class TestConfigWrite(TestConfigSet):
+    module = "config"
+    exp = ConfigFileWriteValues()
+    
+    def setUp(self):
+        TestConfigSet.setUp(self)
+
+        filename = "/tmp/func-test/test2.conf"
+        f = open(filename, "a+")
+        
+        self.cfg.write(f, "main")
+        f.close()
+
+        self.cfg = config.read_config(filename, ConfigTest)
+
+        
+
 
 # test cases
 # open file
