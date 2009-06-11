442f71c8b640b163b4904cb7717d438267b76bea
makkalot <makkalot@fedorabig.localdomain>
2009-06-11 18:56:06 +0300

    global groups api changes nothing is tested yet so probably will have lots of mess to fix

--- func/overlord/groups.py
+++ func/overlord/groups.py
@@ -1,3 +1,10 @@
+from func.overlord.client import Minions
+import fnmatch
+
+def get_hosts_spec(spec):
+    m = Minions(spec)
+    return m.get_hosts_for_spec(spec)
+
 class Groups(object):
 
     def __init__(self, filename=None):
@@ -40,19 +47,37 @@ class Groups(object):
                     hosts.append(host.strip())
 
         return hosts
-
     
+    def add_group(self,group_name,save=False):
+        """
+        Adding a new group
+        """
+        return self.backend.add_group(group_name,save) 
+
+   
+    def add_hosts_to_group_glob(self,group,hoststring,save=True,exclude_string=None):
+        """
+        With that method we will be able add lots of machines by single
+        glob string ...
+        """
+        hoststring = get_hosts_spec(hoststring)
+        if exclude_string :
+            e_s = get_hosts_spec(exclude_string)
+            hoststring = hoststring.difference(e_s)
+
+        #add them to backend
+        self.add_host_list(group,hoststring,save)
+
     def add_hosts_to_group(self, group, hoststring, save = False):
         """
         Here you can add more than one hosts to a given group
         """
         hosts = self.__parse_strings(hoststring)
 
+
         for host in hosts:
             self.add_host_to_group(group, host,save)
-        
-        if save:
-            self.save_changes()
+        self.save_changes()
 
     def add_host_to_group(self, group, host, save = False):
         """
@@ -72,26 +97,13 @@ class Groups(object):
         for host in host_list:
             self.add_host_to_group(group, host)
         
-        if save:
-            self.save_changes()
-
-    def add_subgroups_to_group(self, group, subgroupstring, save = False):
-        """
-        Here you can add more than one subgroup to a given group
-        """
-        subgroups = self.__parse_strings(subgroupstring)
-
-        for subgroup in subgroups:
-            self.add_subgroup_to_group(group, subgroup)
-
-        if save:
-            self.save_changes()
+        self.save_changes()
 
     def add_subgroup_to_group(self, group, subgroup, save = False):
         """
         Add a single subgroup to group
         """
-        self.backend.add_subgroups_to_group(group,subgroup,save)
+        self.backend.add_subgroup_to_group(group,subgroup,save)
     
     def add_subgroup_list(self,group,subgroup_list, save = False):
         """
@@ -108,17 +120,17 @@ class Groups(object):
         if save:
             self.save_changes()
 
-    def get_groups(self):
+    def get_groups(self,pattern=None,exact=True,exclude=None):
         """
         Simple getter
         """
-        return self.__groups
+        return self.backend.get_groups
 
-    def get_subgroups(self):
+    def get_hosts(self,pattern=None,group=None,exact=True,exclude=None):
         """
-        Simple getter
+        Getting the hosts in a proper pattern or what is
         """
-        return self.__subgroups
+        pass
 
     def get_group_names(self):
         """
@@ -154,67 +166,73 @@ class Groups(object):
 
         return unique_hosts
 
-    def save_changes(self):
-        """
-        Write changes to disk
-        """
-        self.backend.save_changes()
-
+   
     def remove_group(self,group_name,save=False):
         """
         Removing a group if needed
         """
         return self.backend.remove_group(group_name,save)
 
+    def remove_group_glob(self,group_str,save=False):
+        """
+        Removing group via a glob
+        """
+        #firstly get all groups availible
+        all_groups = self.get_groups()
+        remove_groups = fnmatch.filter(all_groups,group_str)
+
+    def remove_group_list(self,group_list,save = False):
+        """
+        Removes a group of list
+        """
+
+        if type(host_list) != list:
+            sys.stderr.write("We accept only lists for for remove_group_list method")
+            return False
+        
+        for g in group_list:
+            self.remove_group(g)
+        self.save_changes()
+
+
     def remove_host(self,group_name,host,save=False):
         """
         Removes a proper host from the conf file
         """
         return self.backend.remove_group(group_name,host,save)
+    
+    
+    def remove_host_glob(self,group_name,host_str,save=False,exclude_string=None):
+        host_str = get_hosts_spec(host_str)
+        if exclude_string:
+            e_s = get_hosts_spec(exclude_string)
+            host_str = host_str.difference(e_s)
 
-    def remove_subgroup(self, group_name, subgroup, save=False):
-        """
-        Removes a proper subgroup from the conf file
-        """
-        return self.backend.remove_subgroup(group_name,host,save)
-        
+        #remove the list completely
+        self.remove_host_list(group_name,host_str,save)    
+
     def remove_host_list(self,group,host_list,save = False):
         """
         Remove a whole list from the conf file of hosts
         """
 
-        if type(host_list) != list:
+        if type(host_list) != list or type(host_list) != set :
             sys.stderr.write("We accept only lists for for remove_host_list method")
             return False
 
         for host in host_list:
             self.remove_host(group,host)
 
-        if save:
-            self.save_changes()
+        self.save_changes()
 
-    def remove_subgroup_list(self, group, subgroup_list, save = False):
+
+    def save_changes(self):
         """
-        Remove a whole list from the conf file of subgroups
+        Write changes to disk
         """
+        self.backend.save_changes()
 
-        if type(subgroup_list) != list:
-            sys.stderr.write("We accept only lists for for remove_subgroup_list method")
-            return False
-
-        for subgroup in subgroup_list:
-            self.remove_subgroup(group, subgroup)
-
-        if save:
-            self.save_changes()
-
-
-    def add_group(self,group_name,save=False):
-        """
-        Adding a new group
-        """
-        return self.backend.add_group(group_name,save) 
-
+    
 def main():
     Groups("/tmp/testgroups").show()
 
